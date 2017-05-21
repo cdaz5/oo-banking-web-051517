05e7e755d80192c320527d4f11da241fddf095f9
github username <github email address>
2017-05-21 18:52:05 -0400

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,49 @@
+require 'pry'
 class Transfer
-  # your code here
+
+attr_accessor :sender, :receiver, :amount, :status
+
+def initialize(sender, receiver, amount)
+  @sender = sender
+  @receiver = receiver
+  @amount = amount
+  @status = "pending"
 end
+
+def valid?
+  if sender.valid? == true && receiver.valid? == true
+    true
+  else
+    false
+  end
+end
+
+def execute_transaction
+  if valid? && sender.balance > amount && self.status == "pending"
+    self.sender.balance -= self.amount
+    self.receiver.balance += self.amount
+    self.sender.status = "complete"
+    self.receiver.status = "complete"
+    self.status = "complete"
+  else
+    self.status = "rejected"
+    "Transaction rejected. Please check your account balance."
+  end
+end
+
+def reverse_transfer
+  if execute_transaction
+  sender.balance += self.amount
+  receiver.balance -= self.amount
+  self.sender.status = "reversed"
+  self.receiver.status = "reversed"
+  self.status = "reversed"
+  # binding.pry
+end
+end
+
+
+
+
+
+end ### END OF TRANSFER CLASS ###
